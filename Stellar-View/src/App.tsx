1f4966bf2cd227234ff4028daa37908e3b6ed751
Caden McArthur <caden.mcarthur@gmail.com>
2025-10-04 09:29:25 -0500

    initial commit

---
+++ Stellar-View/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import './App.css'
+
+function App() {
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
---
+++ Stellar-View/vite.config.ts
@@ -0,0 +1,64 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tailwindcss from '@tailwindcss/vite';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(),
+        tailwindcss()],
+    server:{
+        proxy:{
+            '/gibs': {
+                target: 'https://gibs.earthdata.nasa.gov',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/gibs/, ''),
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    optimizeDeps: {
+        exclude: ['cesium'],
+        include: [
+            'mersenne-twister',
+            'urijs',
+            'urijs/src/URI',
+            'grapheme-splitter',
+            'pako',
+            'draco3d',
+            'ktx-parse',
+            'bitmap-sdf',
+            'lerc',
+            'nosleep.js',
+        ],
+        esbuildOptions: {
+            target: 'esnext'
+        }
+    },
+    define: {
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify('./cesium/')
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        rollupOptions: {
+            external: ['cesium'],
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom']
+                },
+                globals: {
+                    cesium: 'Cesium'
+                }
+            }
+        },
+        chunkSizeWarningLimit: 3000,
+        sourcemap: false // Reduces build size significantly
+    }
+
+})
